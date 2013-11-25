29989115f0ce872e73ca7b0a8d44fcbe95566e4a
gunglee <leegf02@163.com>
2013-11-25 19:48:21 +0800

    dcn 分类、分类列表、筛选

---
+++ YiPei/Cell/FenLeiCell.m
@@ -0,0 +1,40 @@
+
+#import "FenLeiCell.h"
+
+@implementation FenLeiCell
+@synthesize titleLabel,arrowImageView,showImageView;
+
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)changeArrowWithUp:(BOOL)up
+{
+    if (up) {
+        self.arrowImageView.image = [UIImage imageNamed:@"UpAccessory.png"];
+    }else
+    {
+        self.arrowImageView.image = [UIImage imageNamed:@"DownAccessory.png"];
+    }
+}
+
+-(void)showImage:(UIImage *)image{
+    self.showImageView.image = image;
+}
+-(void)showImageView:(NSString*)url{
+//    self.showImageView = []
+}
+@end
--- YiPei/fenLeiViewController.h
+++ YiPei/fenLeiViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface fenLeiViewController : UIViewController
-
+@interface fenLeiViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>{
+    NSMutableArray * fenLeiDataArray;//存放分类的数据 数组
+}
+@property(nonatomic, strong) IBOutlet UITableView * fenLeiTableView;
+@property(nonatomic, strong) NSMutableArray * fenLeiDataArray;//存放分类的数据 数组
 @end
